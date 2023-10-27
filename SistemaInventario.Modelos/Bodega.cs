45e4992ef97cf72e9934a7985e4dfb345807f894
Erick <erickgp51@gmail.com>
2023-10-27 15:42:36 -0600

    Configuración del proyecto

---
+++ SistemaInventario.Modelos/Bodega.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaInventario.Modelos
+{
+    public class Bodega
+    {
+
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="El nombre es requerido")]
+        [MaxLength(60, ErrorMessage ="Solo un máximo de 60 caracteres")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "La descripción es requerida")]
+        [MaxLength(60, ErrorMessage = "Solo un máximo de 100 caracteres")]
+        public string Descripcion { get; set; }
+        [Required(ErrorMessage ="El estado es requerido")]
+        public bool Estado { get; set; }
+    }
+}
