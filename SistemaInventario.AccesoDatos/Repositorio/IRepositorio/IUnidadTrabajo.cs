d1967ac5865de93c2571d3f00929b0258e12fdad
Erick <erickgp51@gmail.com>
2023-10-30 19:52:04 -0600

    Código corregido

--- SistemaInventario.AccesoDatos/Repositorio/IRepositorio/IUnidadTrabajo.cs
+++ SistemaInventario.AccesoDatos/Repositorio/IRepositorio/IUnidadTrabajo.cs
@@ -8,9 +8,7 @@ namespace SistemaInventario.AccesoDatos.Repositorio.IRepositorio
 {
     public interface IUnidadTrabajo : IDisposable
     {
-
         IBodegaRepositorio Bodega {  get; }
-
         Task Guardar();
     }
 }
