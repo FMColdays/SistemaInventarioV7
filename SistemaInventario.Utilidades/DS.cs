ddf098040d2a0e896c4338d31f64a36c83fb8c65
Erick <erickgp51@gmail.com>
2023-11-09 16:56:36 -0600

    Carro de Compras - Detalle

--- SistemaInventario.Utilidades/DS.cs
+++ SistemaInventario.Utilidades/DS.cs
@@ -5,6 +5,7 @@
         public const string Exitosa = "Exitosa";
         public const string Error = "Error";
         public const string ImagenRuta = @"\img\producto\";
+        public const string ssCarroCompras = "Sesión carro compras";
         public const string Rol_Admin = "Admin";
         public const string Rol_Cliente = "Cliente";
         public const string Rol_Inventario = "Inventario";
