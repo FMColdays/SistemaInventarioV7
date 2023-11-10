b8d587c4525038fe1dbed49245fb62f6d73657d9
Erick <erickgp51@gmail.com>
2023-11-10 17:40:57 -0600

    Orden Administración

---
+++ SistemaInventario.Modelos/ViewModels/OrdenDetalleVM.cs
@@ -0,0 +1,9 @@
+﻿namespace SistemaInventario.Modelos.ViewModels
+{
+    public class OrdenDetalleVM
+    {
+        public Compania Compania {  get; set; }
+        public Orden Orden { get; set; }
+        public IEnumerable<OrdenDetalle> OrdenDetalleLista {  get; set; }
+    }
+}
---
+++ SistemaInventarioV7/Areas/Admin/Controllers/OrdenController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SistemaInventario.AccesoDatos.Repositorio.IRepositorio;
+using SistemaInventario.Modelos;
+using SistemaInventario.Modelos.ViewModels;
+using SistemaInventario.Utilidades;
+using System.Security.Claims;
+
+namespace SistemaInventarioV7.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    public class OrdenController : Controller
+    {
+        private readonly IUnidadTrabajo _unidadTrabajo;
+        [BindProperty]
+        public OrdenDetalleVM ordenDetalleVM { get; set; }
+
+        public OrdenController(IUnidadTrabajo unidadTrabajo)
+        {
+            _unidadTrabajo = unidadTrabajo; 
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Detalle(int id)
+        {
+            ordenDetalleVM = new OrdenDetalleVM()
+            {
+                Orden = await _unidadTrabajo.Orden.ObtenerPrimero(o => o.Id  == id, incluirPropiedades: "UsuarioAplicacion"),
+                OrdenDetalleLista = await _unidadTrabajo.OrdenDetalle.ObtenerTodos(o => o.OrdenId == id, incluirPropiedades: "Producto")
+            };
+
+            return View(ordenDetalleVM);
+        }
+
+        [Authorize(Roles = DS.Rol_Admin)]
+        public async Task<IActionResult> Procesar(int id)
+        {
+            var orden = await _unidadTrabajo.Orden.ObtenerPrimero(o => o.Id == id);
+            orden.EstadoOrden = DS.EstadoEnProceso;
+            await _unidadTrabajo.Guardar();
+            TempData[DS.Exitosa] = "Orden cambiado a estado en proceso";
+            return RedirectToAction("Detalle", new { id = id});
+        }
+
+
+        [HttpPost]
+        [Authorize(Roles = DS.Rol_Admin)]
+        public async Task<IActionResult> EnviarOrden(OrdenDetalleVM ordenDetalleVM)
+        {
+            var orden = await _unidadTrabajo.Orden.ObtenerPrimero(o => o.Id == ordenDetalleVM.Orden.Id);
+            orden.EstadoOrden = DS.EstadoEnviado;
+            orden.Carrier = ordenDetalleVM.Orden.Carrier;
+            orden.NumeroEnvio = ordenDetalleVM.Orden.NumeroEnvio;
+            orden.FechaEnvio = DateTime.Now;
+            await _unidadTrabajo.Guardar();
+            TempData[DS.Exitosa] = "Orden cambiado a estado enviado";
+            return RedirectToAction("Detalle", new { id = ordenDetalleVM.Orden.Id });
+        }
+
+        #region API
+        [HttpGet]
+        public async Task<IActionResult> ObtenerOrdenLista(string estado)
+        {
+            var claimIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            IEnumerable<Orden> todos;
+            if (User.IsInRole(DS.Rol_Admin)) //validar rol del usuario
+            {
+                todos = await _unidadTrabajo.Orden.ObtenerTodos(incluirPropiedades: "UsuarioAplicacion");
+            }
+            else
+            {
+                todos = await _unidadTrabajo.Orden.ObtenerTodos(o => o.UsuarioAplicacionId == claim.Value ,incluirPropiedades: "UsuarioAplicacion");
+            }
+
+            //validar estado
+            switch (estado)
+            {
+                case "aprobado":
+                    todos = todos.Where(o => o.EstadoOrden == DS.EstadoAprobado);
+                    break;
+                case "completado":
+                    todos = todos.Where(o => o.EstadoOrden == DS.EstadoEnviado);
+                    break;
+                default:
+                    break;
+            }
+
+
+           
+            return Json(new {data = todos});    
+        }
+        #endregion
+    }
+}
